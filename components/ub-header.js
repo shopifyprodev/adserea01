d197a069959ace6a771b5a9c6be50bae82d62c13
TIS-24 <shubham.kale@techinfini.com>
2022-02-25 15:32:08 +0530

    make it better

--- components/ub-header.js
+++ components/ub-header.js
@@ -1,18 +1,18 @@
 import Logoimg from '../public/img/logo.svg';
-import Image from 'next/image'
-
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Header() {
 
   return (
       <header className='shadow-sm sticky-top bg-white'>
       <div className="d-flex flex-column flex-md-row align-items-center py-3 container px-0">
-        <a href="/" className="d-flex align-items-center text-dark text-decoration-none tg-logo">
+        <Link href="/" className="d-flex align-items-center text-dark text-decoration-none tg-logo">
           <Image className='' loader={({src})=> src} src={Logoimg} alt="" height={45} />
-        </a>
+        </Link>
         <nav className="d-inline-flex mt-2 mt-md-0 ms-md-auto me-3">
           
-          <a className="me-3 py-2 text-decoration-none nav-link" href="/">Dashboard</a>
+          <Link className="me-3 py-2 text-decoration-none nav-link" href="/">Dashboard</Link>
          
          
         </nav>
